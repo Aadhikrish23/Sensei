c42a617f25155db7ef3e1f6bfff45ccbee1a2356
Aadhikrish23 <aadhikrish2307@gmail.com>
2026-03-12 21:03:20 +0530

    fix: import AppError in auth middleware

--- Backend/src/middlewares/auth.middleware.ts
+++ Backend/src/middlewares/auth.middleware.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
+import { AppError } from "../utils/AppError.js";
 
 interface JwtPayload {
    id: string;
