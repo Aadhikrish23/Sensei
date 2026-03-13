176e30c24d408edf65842709756f3a70c0839fa9
Aadhikrish23 <aadhikrish2307@gmail.com>
2026-03-13 15:54:21 +0530

    fix: axios timeout

--- Backend/src/lib/aiClient.ts
+++ Backend/src/lib/aiClient.ts
@@ -4,7 +4,7 @@ const baseurl = process.env.AI_SERVICES_URL || "http://localhost:8000"
 
 const aiClient = axios.create({
     baseURL:baseurl,
-    timeout:10000
+    timeout:60000
 })
 
 export default aiClient
